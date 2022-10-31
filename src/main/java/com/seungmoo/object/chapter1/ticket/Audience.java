d8e895caab7b3dcd405b25af0cf6bc4570d1aa39
dltmdan92 <dltmdan92@naver.com>
2022-10-31 21:54:11 +0900

    객체 캡슐화 적용

--- src/main/java/com/seungmoo/object/chapter1/ticket/Audience.java
+++ src/main/java/com/seungmoo/object/chapter1/ticket/Audience.java
@@ -1,12 +1,21 @@
 package com.seungmoo.object.chapter1.ticket;
 
-import lombok.Getter;
-
-@Getter
 public class Audience {
     private Bag bag;
 
     public Audience(Bag bag) {
         this.bag = bag;
     }
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
 }
