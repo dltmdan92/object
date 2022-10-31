2e27460759f2191b65ab099b76d8c5cc2ae591e0
dltmdan92 <dltmdan92@naver.com>
2022-10-31 22:28:18 +0900

    객체 캡슐화 추가 적용 + trade-off 고려

--- src/main/java/com/seungmoo/object/chapter1/ticket/TicketSeller.java
+++ src/main/java/com/seungmoo/object/chapter1/ticket/TicketSeller.java
@@ -8,6 +8,11 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
+        /*
+        TicketOffice는 왜 캡슐화를 안했을 까?
+        - ticketOffice를 캡슐화하게 되면 Audience라는 의존성이 TicketOffice에 추가된다.
+        - 자율화/캡슐화 vs 의존성 추가  -> trade-off를 고려할 것
+         */
         ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
